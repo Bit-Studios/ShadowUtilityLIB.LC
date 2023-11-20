8d4a8675e16ef9de518abb6afce319ab8245b003
Nathan <37817019+NathanPeake@users.noreply.github.com>
2023-11-20 16:25:42 +0000

    Add project files.

---
+++ src/AssetManager.cs
@@ -0,0 +1,105 @@
+﻿using Logger = ShadowUtilityLIB.logging.Logger;
+using UnityEngine;
+
+namespace ShadowUtilityLIB
+{
+    public static class AssetManager
+    {
+        private static Logger logger = new Logger(ShadowLIB.ModId, ShadowLIB.ModVersion);
+        private static Dictionary<string,Texture2D> texture2Ds = new Dictionary<string, Texture2D>();
+        public static Texture2D GetAsset(string ID)
+        {
+            Texture2D AssetData = texture2Ds[ID];
+            return AssetData;
+        }
+        public static void SetAsset(string ID, Texture2D AssetData)
+        {
+            texture2Ds[ID] = AssetData;
+        }
+        public static void LoadAsset(string ID, string Path)
+        {
+            try
+            {
+
+
+                if (Path.EndsWith(".png") || Path.EndsWith(".jpg"))
+                {
+                    System.Drawing.Image img = System.Drawing.Image.FromFile(Path);
+
+                    try{
+                        texture2Ds.Remove(ID);
+                    } catch(Exception e){}
+                        
+                    
+                    texture2Ds.Add(ID, new Texture2D(img.Size.Width, img.Size.Height));
+
+                    texture2Ds[ID].LoadImage(File.ReadAllBytes(Path));
+
+                    texture2Ds[ID].name = ID;
+                }
+            }
+            catch (Exception e)
+            {
+                logger.Error($"{e}\n{e.Message}\n{e.InnerException}\n{e.Source}\n{e.Data}\n{e.HelpLink}\n{e.HResult}\n{e.StackTrace}\n{e.TargetSite}\n{e.GetBaseException()}");
+            }
+
+        }
+        public static void LoadAllAssets()
+        {
+            try
+            {
+                List<string> DirectoriesToCheck = new List<string>();
+                List<string> ModPathsToCheck = new List<string>();
+                void LoadDirectory(string path)
+                {
+                    logger.Debug($"{path}");
+                    foreach (var DirPath in Directory.GetDirectories(path))
+                    {
+                        DirectoriesToCheck.Add($"{DirPath}");
+                        logger.Debug($"{DirPath}");
+                    };
+                }
+                void LoadModPath(string path)
+                {
+                    logger.Debug($"{path}");
+                    foreach (var ModPath in Directory.GetDirectories(path))
+                    {
+                        ModPathsToCheck.Add($"{ModPath}");
+                        logger.Debug($"{ModPath}");
+                    };
+                }
+                void LoadAssetsInFolder(string path)
+                {
+                    if (path.ToLower().EndsWith("assets"))
+                    {
+                        logger.Debug($"assets {path}");
+                        if (Directory.GetDirectories(path).Contains($"{path}\\images"))//Image Assets convert to Texture2D
+                        {
+                            foreach (var file in Directory.GetFiles($"{path}\\images"))
+                            {
+                                logger.Debug($"{file}");
+                                LoadAsset(Path.GetFileName(file), $"{file}");
+                            }
+                        }
+                    }
+                }
+                if (Directory.Exists("./BepInEx/plugins"))
+                {
+                    LoadDirectory("./BepInEx/plugins");
+                }
+                foreach (string path in DirectoriesToCheck)
+                {
+                    LoadModPath(path);
+                }
+                foreach (string path in ModPathsToCheck)
+                {
+                    LoadAssetsInFolder(path);
+                }
+            }
+            catch (Exception e)
+            {
+                logger.Error($"{e}\n{e.Message}\n{e.InnerException}\n{e.Source}\n{e.Data}\n{e.HelpLink}\n{e.HResult}\n{e.StackTrace}\n{e.TargetSite}\n{e.GetBaseException()}");
+            }
+        }
+    }
+}
---
+++ src/ShadowUtilityLIBMod.cs
@@ -0,0 +1,124 @@
+﻿using Logger = ShadowUtilityLIB.logging.Logger;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using ShadowUtilityLIB.UI;
+using HarmonyLib;
+using UnityEngine.UIElements;
+using BepInEx;
+using ShadowUtilityLIB.logging;
+using System.Reflection;
+
+namespace ShadowUtilityLIB;
+public class CoroutineExecuter : MonoBehaviour { 
+    public void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
+
+[BepInPlugin("com.shadowdev.utilitylib", "shadowutilitylib", ShadowLIB.ModVersion)]
+public sealed class ShadowUtilityLIBMod : BaseUnityPlugin
+{
+    public const string ModId = ShadowLIB.ModId;
+    public const string ModVersion = ShadowLIB.ModVersion;
+    public static bool IsDev = ShadowLIB.IsDev;
+    public static bool Initilised = ShadowLIB.Initilised;
+    void Awake()
+    {
+        ShadowLIB.SoftDependancys["BepInEx"] = true;
+        ShadowLIB.Awake();
+        
+    }
+
+    //compatibility with older versions
+    public static void EnableDebugMode()
+    {
+        ShadowLIB.EnableDebugMode();
+    }
+    public static void RunCr(IEnumerator cr)
+    {
+        ShadowLIB.RunCr(cr);
+    }
+}
+
+
+//universal
+public static class ShadowLIB
+{
+    public const string ModId = "com.shadowdev.utilitylib";
+    public const string ModVersion = "0.0.9";
+    public static bool IsDev = false;
+    private static Logger logger = new Logger(ModId, ModVersion);
+    private static CoroutineExecuter instance;
+    public static bool Initilised = false;
+    public static Dictionary<string, bool> SoftDependancys = new Dictionary<string, bool>() {{ "BepInEx", false } };
+    public static bool CRE = false;
+    public static void Awake()
+    {
+        EnableDebugMode();
+        try
+        {
+            if (!CRE)
+            {
+                CRE = true;
+                instance = new GameObject("CoroutineExecuter").AddComponent<CoroutineExecuter>();
+            }
+            
+        }
+        catch (Exception e)
+        {
+            logger.Error($"{e}\n{e.Message}\n{e.InnerException}\n{e.Source}\n{e.Data}\n{e.HelpLink}\n{e.HResult}\n{e.StackTrace}\n{e.TargetSite}\n{e.GetBaseException()}");
+        }
+        LogManager.StartLogManager();
+        AssetManager.LoadAllAssets();
+        try
+        {
+            Harmony.CreateAndPatchAll(typeof(DropdownUtils));
+        }
+        catch (Exception e)
+        {
+            logger.Error($"{e}\n{e.Message}\n{e.InnerException}\n{e.Source}\n{e.Data}\n{e.HelpLink}\n{e.HResult}\n{e.StackTrace}\n{e.TargetSite}\n{e.GetBaseException()}");
+        }
+        
+        
+
+        logger.Log($"Initialized");
+        Initilised = true;
+    }
+    public static void EnableDebugMode()
+    {
+        IsDev = true;
+        logger.Debug("Debug Mode Enabled");
+    }
+    public static void RunCr(IEnumerator cr)
+    {
+        try
+        {
+            instance.StartCoroutine(cr);
+        }
+        catch (Exception e)
+        {
+            logger.Error($"{e}\n{e.Message}\n{e.InnerException}\n{e.Source}\n{e.Data}\n{e.HelpLink}\n{e.HResult}\n{e.StackTrace}\n{e.TargetSite}\n{e.GetBaseException()}");
+            if (!CRE)
+            {
+                CRE = true;
+                instance = new GameObject("CoroutineExecuter").AddComponent<CoroutineExecuter>();
+                RunCr(cr);
+            }
+        }
+
+    }
+    public static bool NamespaceExists(string desiredNamespace)//https://forum.unity.com/threads/run-bit-of-code-if-namespace-exists-c.437745/
+    {
+        foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+        {
+            foreach (Type type in assembly.GetTypes())
+            {
+                if (type.Namespace == desiredNamespace)
+                    return true;
+            }
+        }
+        return false;
+    }
+}
